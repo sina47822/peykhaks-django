bf9791d5fef851a88a5a581f5eb0b333f101ad93
sina <sinaa.afshar@gmail.com>
2025-01-03 14:03:42 +0330

    edit price list and add tag clarity hot map

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -254,24 +254,24 @@ $(document).ready(function(){
   })
 });
 
-// switch price list checked box
-$(document).ready(function(){
+// // switch price list checked box
+// $(document).ready(function(){
 
-  $("#flexSwitchCheckChecked").on('change', function() {
-    if ($(this).is(':checked')) {
-        $('.price-list-beton').attr('style', 'display : block');
-        $('.price-list-khak').attr('style', 'display : none');
-        $('#flexSwitchCheckCheckedLable').text('برای تغییر لیست از بتن به خاک روی سوییچ کلیک کنید')
+//   $("#flexSwitchCheckChecked").on('change', function() {
+//     if ($(this).is(':checked')) {
+//         $('.price-list-beton').attr('style', 'display : block');
+//         $('.price-list-khak').attr('style', 'display : none');
+//         $('#flexSwitchCheckCheckedLable').text('برای تغییر لیست از بتن به خاک روی سوییچ کلیک کنید')
 
-    }
-    else {
-       $('.price-list-beton').attr('style', 'display : none');
-       $('.price-list-khak').attr('style', 'display : block');
-       $('#flexSwitchCheckCheckedLable').text('برای تغییر لیست از خاک به بتن روی سوییچ کلیک کنید')
+//     }
+//     else {
+//        $('.price-list-beton').attr('style', 'display : none');
+//        $('.price-list-khak').attr('style', 'display : block');
+//        $('#flexSwitchCheckCheckedLable').text('برای تغییر لیست از خاک به بتن روی سوییچ کلیک کنید')
 
 
-    }});
-});
+//     }});
+// });
 
 "use strict";
 !function () {
